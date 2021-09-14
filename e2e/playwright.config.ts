38df448978c541685989e556a7705be87b643e34
Mark Skelton <mskelton@widen.com>
2021-09-14 10:58:34 -0500

    Add tracing

--- e2e/playwright.config.ts
+++ e2e/playwright.config.ts
@@ -3,9 +3,9 @@ import { PlaywrightTestConfig } from '@playwright/test'
 const config: PlaywrightTestConfig = {
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  testMatch: /root/,
   use: {
     screenshot: 'only-on-failure',
+    trace: 'on-first-retry',
   },
 }
 
