8878bf055744126a41a1f0a38d1d946adbb091fb
Mark Skelton <mskelton@widen.com>
2021-09-20 15:23:15 -0500

    Remove `Locator` export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,1 @@
 export { Collection, ElementOptions } from './Collection'
-
-// We need to export the Locator type otherwise TypeScript will complain about
-// that inferred types cannot be named with a reference to playwright-core.
-export type { Locator } from 'playwright-core'
