6118eb741a7fefb65fce82a52f77b3d801eb99b2
Mark Skelton <mark.skelton@acquia.com>
2023-01-27 15:37:16 -0600

    Compile with tsup

--- scripts/release.sh
+++ scripts/release.sh
@@ -2,9 +2,9 @@
 set -eo pipefail
 
 if [[ -z "${CI}" ]]; then
-  echo "Releasing is only allowed from CI."
-  exit 1
+	echo "Releasing is only allowed from CI."
+	exit 1
 fi
 
-yarn tsc -p tsconfig.build.json
+yarn build
 yarn changeset publish
